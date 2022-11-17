b2e06bb4b43ee1f6a7939d2f9be843fc88badba6
Polarys Fundation <polarysfundation@gmail.com>
2022-11-17 15:08:50 -0400

    optimization when obtaining the gasFee

--- src/components/Mint/Mint.jsx
+++ src/components/Mint/Mint.jsx
@@ -1,129 +1,132 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React, { useState } from 'react'
-import { ethers, BigNumber } from 'ethers'
-import charityDogsClub from '../Web3/abi.json'
-import '../css/media.css'
-import './Mint.css'
-import '../css/mediakit.css'
-import banner from '../../images/Banner0024.png'
-import banner2 from '../../images/2036569998.png'
+import React, { useState } from "react";
+import { ethers, BigNumber } from "ethers";
+import charityDogsClub from "../Web3/abi.json";
+import "../css/media.css";
+import "./Mint.css";
+import "../css/mediakit.css";
+import banner from "../../images/Banner0024.png";
+import banner2 from "../../images/2036569998.png";
 
+const charityDogsClubAddress = "0x053abB888946d56635f795DDA97e6a7fF6217Fd5";
 
-const charityDogsClubAddress = "0x053abB888946d56635f795DDA97e6a7fF6217Fd5"
+const Mint = ({ accounts, setAccounts }) => {
+  const [mintAmount, setMintAmount] = useState(1);
+  const [supply, setSupply] = useState();
 
-const Mint = ({accounts, setAccounts}) => {
+  const isConnected = Boolean(accounts[0]);
 
+  async function getSupply() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
 
-    const [mintAmount, setMintAmount] = useState(1);
-    const [supply, setSupply] = useState()
+      const contract = new ethers.Contract(
+        charityDogsClubAddress,
+        charityDogsClub,
+        signer
+      );
 
-    const isConnected = Boolean(accounts[0]);
+      try {
+        const response = await contract.totalSupply();
 
-    async function getSupply (){
-      if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner()
-        
-        const contract = new ethers.Contract(
-          charityDogsClubAddress,
-          charityDogsClub,
-          signer
+        setSupply(Number(response));
 
-        );
-
-        try {
-          const response = await contract.totalSupply();
-
-          setSupply(Number(response))
-
-    
-
-          console.log("response: ", Number(response))
-        }
-
-        catch (err){
-          console.log("error: ", err)
-        }
-      }
-    };
-
-    getSupply()
-
-
-
-    async function handleMint (){
-      if (window.ethereum) {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-
-        const contract = new ethers.Contract(
-          charityDogsClubAddress,
-          charityDogsClub,
-          signer
-          
-        ); 
-
-        try {
-
-          const response = await contract.buy(BigNumber.from(mintAmount),
-
-          {
-
-            gasLimit: 200000,
-            value: ethers.utils.parseEther((0.04 * mintAmount).toString())
-          });
-          console.log("response: ", response);
-        } 
-        catch (err){
-          console.log("error: ", err)
-        }
+        console.log("response: ", Number(response));
+      } catch (err) {
+        console.log("error: ", err);
       }
     }
+  }
 
-    const handleDecrement = () => {
-      if (mintAmount <= 1) return;
-      setMintAmount(mintAmount - 1);
-    };
+  getSupply();
 
-    const handleIncrement = () => {
-      if (mintAmount >= 10) return;
-      setMintAmount(mintAmount + 1);
-    };
+  async function handleMint() {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(
+        charityDogsClubAddress,
+        charityDogsClub,
+        signer
+      );
+
+      try {
+        const getGas = async (e) => {
+          try {
+            const getPrice = await contract.price();
+            const response = await contract.buy(BigNumber.from(mintAmount), {
+              gasLimit: e,
+              value: (getPrice * mintAmount).toString(),
+            });
+            console.log("response: ", JSON.stringify(response));
+          } catch (err) {
+            console.log("error: " + JSON.stringify(err));
+          }
+        };
+        getGas();
+      } catch (err) {
+        console.log("error: ", err);
+      }
+    }
+  }
+
+  const handleDecrement = () => {
+    if (mintAmount <= 1) return;
+    setMintAmount(mintAmount - 1);
+  };
+
+  const handleIncrement = () => {
+    if (mintAmount >= 10) return;
+    setMintAmount(mintAmount + 1);
+  };
 
   return (
-    <div className="container-mint" id="Mint" >
-        <div className="container-mint2">
-          <div className="container-mint3">
-            <div className="container-mint4">
+    <div className="container-mint" id="Mint">
+      <div className="container-mint2">
+        <div className="container-mint3">
+          <div className="container-mint4">
+            <div className="container-mint7 fadeIn">{supply}/5555 </div>
+            <img src={banner} alt="" className="mint-img fadeIn" />
+            <div className="container-mint6 fadeIn">
+              <button onClick={handleDecrement} className="mint-button">
+                -
+              </button>
 
-              <div className='container-mint7 fadeIn' >{supply}/5555 </div>
-                <img src={banner} alt="" className="mint-img fadeIn" />
-                  <div className="container-mint6 fadeIn">
-                    <button onClick={handleDecrement} className="mint-button">-</button>
+              <input
+                type="number"
+                readOnly
+                value={mintAmount}
+                className="mint-input"
+              />
 
-                    <input type="number" readOnly value={mintAmount} className="mint-input" />
-
-
-                    <button onClick={handleIncrement} className="mint-button">+</button>
-
-                  </div>
-
-              {isConnected ? (
-                <div className="container-mint5 fadeIn">
-                  <button onClick={handleMint} className="mint-button2">Mint</button>
-                </div>
-              ) : (
-                  <p className="mint-p fadeIn">You must be connected to Mint</p>
-              )}
+              <button onClick={handleIncrement} className="mint-button">
+                +
+              </button>
             </div>
+
+            {isConnected ? (
+              <div className="container-mint5 fadeIn">
+                <button onClick={handleMint} className="mint-button2">
+                  Mint
+                </button>
+              </div>
+            ) : (
+              <p className="mint-p fadeIn">You must be connected to Mint</p>
+            )}
           </div>
-                <div className="container-mint8 fadeIn">
-                  <h2 className="mint-h2">First Minting Phase</h2>
-                    <p className="mint-p3">Our first minting phase grants its holders OG passes, a total of 1,111 OG Pups, as well as many future benefits from our projects.</p>
-                  <img src={banner2} alt="Banner-2" className="mint-img2 fadeIn" />
-                </div>
+        </div>
+        <div className="container-mint8 fadeIn">
+          <h2 className="mint-h2">First Minting Phase</h2>
+          <p className="mint-p3">
+            Our first minting phase grants its holders OG passes, a total of
+            1,111 OG Pups, as well as many future benefits from our projects.
+          </p>
+          <img src={banner2} alt="Banner-2" className="mint-img2 fadeIn" />
         </div>
       </div>
+    </div>
   );
 };
 
